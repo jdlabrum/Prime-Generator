ad8d8731e89cfa1c60d0443958e5d0a0b74a6201
Joseph Labrum <josephlabrum@yahoo.com>
2015-11-21 15:10:58 -0700

    Adding Files

---
+++ PrimeApp/Form1.Designer.vb
@@ -0,0 +1,106 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lTextbox = New System.Windows.Forms.TextBox()
+        Me.uTextbox = New System.Windows.Forms.TextBox()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(24, 27)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(70, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Lower Bound"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(24, 67)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(70, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Upper Bound"
+        '
+        'lTextbox
+        '
+        Me.lTextbox.Location = New System.Drawing.Point(115, 24)
+        Me.lTextbox.Name = "lTextbox"
+        Me.lTextbox.Size = New System.Drawing.Size(123, 20)
+        Me.lTextbox.TabIndex = 2
+        '
+        'uTextbox
+        '
+        Me.uTextbox.Location = New System.Drawing.Point(115, 64)
+        Me.uTextbox.Name = "uTextbox"
+        Me.uTextbox.Size = New System.Drawing.Size(123, 20)
+        Me.uTextbox.TabIndex = 3
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(27, 112)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(211, 121)
+        Me.ListBox1.TabIndex = 4
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(162, 253)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 5
+        Me.Button1.Text = "Generate"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(268, 291)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.uTextbox)
+        Me.Controls.Add(Me.lTextbox)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Prime Generator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents lTextbox As System.Windows.Forms.TextBox
+    Friend WithEvents uTextbox As System.Windows.Forms.TextBox
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+
+End Class
---
+++ PrimeApp/Form1.vb
@@ -0,0 +1,52 @@
+﻿Public Class Form1
+
+    Dim lBound As Integer
+    Dim ubound As Integer
+    Dim odds As Integer = 3
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ListBox1.Items.Clear()
+        Try
+            If lTextbox.Text > uTextbox.Text Then
+                Throw New System.Exception("Invalid value error")
+            End If
+
+            For i As Integer = lTextbox.Text To uTextbox.Text
+                isPrime(i)
+            Next
+
+            If ListBox1.Items.Count = 0 Then
+                Throw New System.Exception("No value error")
+            End If
+
+        Catch ex As InvalidCastException
+            ListBox1.Items.Add("Please enter an integer in each field")
+        Catch ex As Exception
+            ListBox1.Items.Add("No prime numbers within range")
+        End Try
+
+    End Sub
+
+    Public Sub isPrime(ByVal input As Integer)
+        If input < 2 Then
+            Return
+        ElseIf input = 2 Then
+            ListBox1.Items.Add(input)
+            Return
+        ElseIf (input Mod 2) = 0 Then
+            Return
+        Else
+            odds = 3
+            While odds < input
+                If input Mod odds = 0 Then
+                    Return
+                End If
+                odds += 2
+            End While
+            ListBox1.Items.Add(input)
+        End If
+        Return
+    End Sub
+
+End Class
